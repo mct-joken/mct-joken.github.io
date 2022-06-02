6399b43554059d18fc12bdd3539d98e545c5e714
kijiharu <kijiharu3112@gmail.com>
2022-06-02 12:13:26 +0900

    部員一覧

--- src/pages/Memberlist.svelte
+++ src/pages/Memberlist.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
   import Container from "../components/Container.svelte";
+  import SvelteMarkdown from 'svelte-markdown';
+
+  const source:string = `
+  ## 部長  J3 山本崇人
+  - 3年 6人
+  - 2年 3人
+  - 1年 5人
+  `
 </script>
 
 <Container title={"部員一覧"} color={"bg-teal-200"}/>
 <div class="mx-auto w-5/6">
-  <h3>仮入部期間中のため準備中</h3>
+  <SvelteMarkdown {source}/>
 </div>
