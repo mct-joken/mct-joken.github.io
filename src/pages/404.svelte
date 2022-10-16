20fdd7ef1b71e595597dfef38bdbd8d590d2f12f
kijiharu <kijiharu@kijiharus-MacBook-Air.local>
2022-10-16 21:49:04 +0900

    404ページに遷移したら自動的にトップページに遷移するようにした.

--- src/pages/404.svelte
+++ src/pages/404.svelte
@@ -1,3 +1,22 @@
-<h1>
-  ページが見つかりませんでした
-</h1>
+<script lang="ts">
+  import {push} from 'svelte-spa-router';
+  setTimeout(() => {
+    push(`/`)
+  }, 3000);
+</script>
+<h1>ページが見つかりませんでした</h1>
+<h2>
+  3秒後に自動的にトップページに戻ります
+</h2>
+<style>
+  h1{
+    font-size: large;
+    text-align: center;
+  }
+  h2{
+    text-align: center;
+    font-size: medium;
+  }
+</style>
+
+
