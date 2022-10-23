3a902056825687c3b1df893670359e3878db071f
kijiharu <kijiharu@kijiharus-MacBook-Air.local>
2022-10-23 20:26:53 +0900

    footerの追加

---
+++ src/components/Footer.svelte
@@ -0,0 +1,25 @@
+<footer class="w-full z-40 bg-gray-900 mx-auto rounded-bl rounded-br shadow border-b border-gray-200">
+    <div class="container px-40 flex justify-between items-center">
+      <span class="font-semibold w-full text-xl text-white tracking-tight centering_item">
+        <p>連絡先 : me(at)laminne33569.net</p>
+        <p>(atをアットマークに置換してください)</p>
+        <ul style="display: flex; justify-content: center">
+          <li class="github"> <a href="https://github.com/mct-joken" target="_blank"><img style="width: 1.85em; height: 1.85em" src="github_icon.png" alt="github"></a></li>
+          <li class="twitter"><a href="https://twitter.com/mct_joken" target="_blank"><img style="width: 2em; height: 2em" src="twitter_icon.png" alt="twitter"></a></li>
+        </ul>
+        <p>(c)松江高専 情報科学研究部</p>
+      </span>
+  </div>
+</footer>
+
+<style>
+  p {
+    font-size: medium;
+    text-align: center;
+  }
+  ul li {
+    list-style: none;
+    align-items: center;
+  }
+</style>
+
